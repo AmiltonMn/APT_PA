d6de7a6897f370317349575c388c214299e34c77
AmiltonMn <amilton.moreiran@hotmail.com>
2024-09-27 16:58:22 -0300

    commit

--- src/Components/menu.tsx
+++ src/Components/menu.tsx
@@ -1,3 +1,4 @@
+import { ROUTES } from "@/constants/route";
 import Link from "next/link";
 
 // interface IMenu {
@@ -18,14 +19,14 @@ export const Menu = ({op1, op2, op3, op4} : {
 
     const style = 
     {
-        bttn:"px-2 py-1 min-w-3.5 text-branco",
+        bttn:"px-2 py-1 min-w-3.5 text-branco cursor-pointer",
         nav: "text-pink-500 bg-pink-700 gap-3 font-robFont text-medium flex flex-row justify-around align-center p-3"
     }
 
     return (
     <nav className={style.nav}>
-        <p className={style.bttn}>{op1}</p>
-        <p className={style.bttn}>{op2}</p>
+        <Link href={ROUTES.home} className={style.bttn}>{op1}</Link>
+        <Link href={ROUTES.maths} className={style.bttn}>{op2}</Link>
         <p className={style.bttn}>{op4}</p>
         <p className={style.bttn}>{op1}</p>
     </nav>
